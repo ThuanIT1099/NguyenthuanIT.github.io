6f82d982a26a01533091f1b930262d3cee3a036f
Nguyễn M. Thuận IT <161277847+ThuanIT1099@users.noreply.github.com>
2024-12-11 12:15:59 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function validate() {
     if (username == "admin" && password == "123456") {
         swal({
             title: "",
-            text: "Xin chào Võ Trường",
+            text: "Xin chào Nguyễn Thuận IT",
             icon: "success",
             close: true,
             button: false,
